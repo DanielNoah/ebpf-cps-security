bb03a18438e5f2f165c9bafc4854165d2ed6b6cf
Younggi <gonoah8@gmail.com>
2026-05-06 09:05:24 +0900

    Add files via upload
    Add comments updated.

--- LearningC/16-2/16-6.c
+++ LearningC/16-2/16-6.c
@@ -6,10 +6,10 @@ int main(int argc, char **argv)
 {
 	int i;
 
-	while (*argv != NULL)
+	while (*argv != NULL)		// argv가 가리키는 배열 요소의 값이 널 포인터가 아닌동안
 	{
-		printf("%s\n", *argv);
-		argv++;
+		printf("%s\n", *argv);	// 문자열을 출력
+		argv++;					// 포인터 배열의 다음 요소로 이동
 	}
 
 	return 0;
